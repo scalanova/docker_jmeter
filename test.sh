8a24273b61a6fb42fff762de2bda1ddafc19e068
scalanova <scalanova@users.noreply.github.com>
2015-08-21 16:19:59 +0300

    Add "temperature" and "pressure" input support from the user

--- test.sh
+++ test.sh
@@ -10,6 +10,12 @@ echo "Number of threads is: $thread"
 echo "Please enter number of loops..."
 read loop
 echo "Number of loops is: $loop"
+echo "Please enter temperature value..."
+read temperature
+echo "Temperature is: $tempetature"
+echo "Please enter pressure value..."
+read pressure
+echo "Pressure value is: $pressure"
 
 cp OrionTestPlan.jmx Test.jmx
 
@@ -17,6 +23,8 @@ sed -i -e s/_ip_/$ip/g Test.jmx
 sed -i -e s/_port_/$port/g Test.jmx
 sed -i -e s/_thread_/$thread/g Test.jmx
 sed -i -e s/_loop_/$loop/g Test.jmx
+sed -i -e s/_temperature_/$temperature/g Test.jmx
+sed -i -e s/_pressure_/$pressure/g Test.jmx
 
 echo "Running Docker JMeter Instance With Parameters Provided..."
 
